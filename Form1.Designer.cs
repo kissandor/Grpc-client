e90d76c49f7e1e2ff7e21b68998b1c4e1ce76fb0
Sandor Kis <kisandor@hotmail.com>
2023-01-21 01:23:04 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿
+namespace StockClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbtnList = new System.Windows.Forms.Button();
+            this.uidLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxProductQty = new System.Windows.Forms.TextBox();
+            this.txtBoxProductNumber = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxProductName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAddProduct = new System.Windows.Forms.Button();
+            this.userNameTxtBox = new System.Windows.Forms.TextBox();
+            this.passwordTxtBox = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbtnList
+            // 
+            this.lbtnList.Location = new System.Drawing.Point(22, 380);
+            this.lbtnList.Name = "lbtnList";
+            this.lbtnList.Size = new System.Drawing.Size(75, 23);
+            this.lbtnList.TabIndex = 2;
+            this.lbtnList.Text = "List";
+            this.lbtnList.UseVisualStyleBackColor = true;
+            this.lbtnList.Click += new System.EventHandler(this.lbtnList_Click_1);
+            // 
+            // uidLabel
+            // 
+            this.uidLabel.AutoSize = true;
+            this.uidLabel.Location = new System.Drawing.Point(550, 305);
+            this.uidLabel.Name = "uidLabel";
+            this.uidLabel.Size = new System.Drawing.Size(0, 15);
+            this.uidLabel.TabIndex = 5;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.logoutToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            this.logoutToolStripMenuItem.Click += new System.EventHandler(this.logoutToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtBoxProductQty);
+            this.groupBox1.Controls.Add(this.txtBoxProductNumber);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtBoxProductName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnAddProduct);
+            this.groupBox1.Location = new System.Drawing.Point(543, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(215, 171);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add new product";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Available Qty. :";
+            // 
+            // txtBoxProductQty
+            // 
+            this.txtBoxProductQty.Location = new System.Drawing.Point(109, 90);
+            this.txtBoxProductQty.Name = "txtBoxProductQty";
+            this.txtBoxProductQty.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxProductQty.TabIndex = 5;
+            // 
+            // txtBoxProductNumber
+            // 
+            this.txtBoxProductNumber.Location = new System.Drawing.Point(109, 32);
+            this.txtBoxProductNumber.Name = "txtBoxProductNumber";
+            this.txtBoxProductNumber.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxProductNumber.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Product number : ";
+            // 
+            // txtBoxProductName
+            // 
+            this.txtBoxProductName.Location = new System.Drawing.Point(109, 61);
+            this.txtBoxProductName.Name = "txtBoxProductName";
+            this.txtBoxProductName.Size = new System.Drawing.Size(100, 23);
+            this.txtBoxProductName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Product name :";
+            // 
+            // btnAddProduct
+            // 
+            this.btnAddProduct.Location = new System.Drawing.Point(7, 130);
+            this.btnAddProduct.Name = "btnAddProduct";
+            this.btnAddProduct.Size = new System.Drawing.Size(202, 35);
+            this.btnAddProduct.TabIndex = 0;
+            this.btnAddProduct.Text = "Add";
+            this.btnAddProduct.UseVisualStyleBackColor = true;
+            this.btnAddProduct.Click += new System.EventHandler(this.btnAddProduct_Click);
+            // 
+            // userNameTxtBox
+            // 
+            this.userNameTxtBox.Location = new System.Drawing.Point(110, 27);
+            this.userNameTxtBox.Name = "userNameTxtBox";
+            this.userNameTxtBox.Size = new System.Drawing.Size(100, 23);
+            this.userNameTxtBox.TabIndex = 8;
+            // 
+            // passwordTxtBox
+            // 
+            this.passwordTxtBox.Location = new System.Drawing.Point(294, 27);
+            this.passwordTxtBox.Name = "passwordTxtBox";
+            this.passwordTxtBox.Size = new System.Drawing.Size(100, 23);
+            this.passwordTxtBox.TabIndex = 9;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(231, 30);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(63, 15);
+            this.lblPassword.TabIndex = 10;
+            this.lblPassword.Text = "Password: ";
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(33, 30);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(66, 15);
+            this.lblUserName.TabIndex = 11;
+            this.lblUserName.Text = "Username: ";
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Location = new System.Drawing.Point(419, 30);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(75, 23);
+            this.loginBtn.TabIndex = 12;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(498, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "label4";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 71);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(510, 303);
+            this.dataGridView1.TabIndex = 14;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.passwordTxtBox);
+            this.Controls.Add(this.userNameTxtBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.uidLabel);
+            this.Controls.Add(this.lbtnList);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Stock Control App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button lbtnList;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        public System.Windows.Forms.Label uidLabel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxProductQty;
+        private System.Windows.Forms.TextBox txtBoxProductNumber;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxProductName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAddProduct;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.TextBox userNameTxtBox;
+        private System.Windows.Forms.TextBox passwordTxtBox;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using StockClient;
+using Grpc.Core;
+using System.IO;
+
+namespace StockClient
+{
+    public partial class Form1 : Form
+    {
+
+        GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:5001");
+        Stock.StockClient client;
+        static string uid = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            client = new Stock.StockClient(channel);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //client = new Stock.StockClient(channel);
+        }
+
+        private void logoutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Session_Id tempuid = new Session_Id();
+                tempuid.Id = uid;
+                Result res = client.Logout(tempuid);
+                uidLabel.Text = res.ToString();
+            }
+            catch
+            {
+                uidLabel.Text = "Server is offline!";
+            }
+
+        }
+
+        private void btnAddProduct_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                NewItem newItem = new NewItem();
+
+                if (txtBoxProductName.Text == "")
+                    throw new Exception("Missing product name");
+                newItem.Name = txtBoxProductName.Text;
+
+                if (txtBoxProductNumber.Text == "")
+                    throw new Exception("Missing product number");
+                newItem.Code = txtBoxProductNumber.Text;
+
+                if (int.Parse(txtBoxProductQty.Text) < 0)
+                    throw new Exception("Incorrect quantity");
+                newItem.Price = int.Parse(txtBoxProductQty.Text);
+
+                newItem.Uid = uid;
+                Result res = client.ItemAdd(newItem);
+                uidLabel.Text = res.ToString();
+
+                lbtnList_Click_1(sender, e);
+            }
+            catch (Exception exc)
+            {
+                uidLabel.Text = exc.Message;
+            }
+
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (uid != null && uid != "")
+            {
+                logoutToolStripMenuItem_Click(sender, e);
+            }
+            this.Close();
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                User user = new User();
+                user.Name = userNameTxtBox.Text;
+                user.Passwd = passwordTxtBox.Text;
+                Session_Id tempuid = client.Login(user);
+                label4.Text = tempuid.ToString();
+
+                if (tempuid.Id.ToString() != "Login Faild")
+                {
+                    uid = tempuid.ToString().Substring(9, 36);
+                }
+                else
+                {
+                    uid = "";
+                }
+
+                uidLabel.Text = uid;
+            }
+            catch
+            {
+                uidLabel.Text = "Server is offline";
+            }
+        }
+
+        private async void lbtnList_Click_1(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Item number");
+            dt.Columns.Add("Item name");
+            dt.Columns.Add("Qty in stock");
+            dt.Columns.Add("U");
+            dt.Columns.Add("D");
+
+            DataRow dr;
+
+            try
+            {
+                using (var call = client.List(new Empty { }))
+                {
+                    while (await call.ResponseStream.MoveNext())
+                    {
+                        StockItem stockItem = call.ResponseStream.Current;
+                        dr = dt.NewRow();
+                        dr["Item number"] = stockItem.Code.ToString();
+                        dr["Item name"] = stockItem.Name.ToString();
+                        dr["Qty in stock"] = stockItem.CurPrice.ToString();
+                        dr["U"] = "U";
+                        dr["D"] = "D";
+
+                        dt.Rows.Add(dr);
+
+                    }
+                }
+            }
+            catch { uidLabel.Text = "Server is offline!"; }
+
+
+            dataGridView1.DataSource = dt;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Columns[0].ReadOnly = true;
+            dataGridView1.Columns[1].ReadOnly = true;
+            dataGridView1.Columns[3].ReadOnly = true;
+            dataGridView1.Columns[4].ReadOnly = true;
+            dataGridView1.Columns[0].Width = 100;
+            dataGridView1.Columns[1].Width = 270;
+            dataGridView1.Columns[2].Width = 100;
+            dataGridView1.Columns[3].Width = 20;
+            dataGridView1.Columns[4].Width = 20;
+            
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridView1.CurrentCell.ColumnIndex.Equals(4) && e.RowIndex != -1)
+            {
+                if (dataGridView1.CurrentCell != null && dataGridView1.CurrentCell.Value != null) 
+                {
+                    try
+                    {
+                        //MessageBox.Show(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+                        DeleteStockItem toDeleteitem = new DeleteStockItem();
+                        toDeleteitem.Code = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                        toDeleteitem.Uid = uid;
+                        Result res = client.ItemDelete(toDeleteitem);
+                        uidLabel.Text = res.ToString();
+                        lbtnList_Click_1(sender, e);
+                    }
+                    catch(Exception exc)
+                    {
+                        uidLabel.Text = exc.Message;
+                    }
+
+                }
+            }
+
+            if (dataGridView1.CurrentCell.ColumnIndex.Equals(3) && e.RowIndex != -1)
+            {
+                if (dataGridView1.CurrentCell != null && dataGridView1.CurrentCell.Value != null)
+                {
+                    try
+                    {
+                        //MessageBox.Show(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+                        UpdatedStockItem itemToUpdate = new UpdatedStockItem();
+                        itemToUpdate.Code = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+                        itemToUpdate.Price = int.Parse(dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString());
+                        itemToUpdate.Uid = uid;
+                        Result res = client.StockItemUpdate(itemToUpdate);
+                        uidLabel.Text = res.ToString();
+                        lbtnList_Click_1(sender, e);
+                    }
+                    catch (Exception exc)
+                    {
+                        uidLabel.Text = exc.Message;
+                    }
+
+                }
+            }
+        }
+
+    }
+}
